c0c6494790f189cc56717f0714fb724f7db43e73
richardchien6 <richardchien6@yahoo.com.tw>
2019-10-17 11:42:11 +0800

    伺服器端，客戶端

--- 駭客/server.py
+++ 駭客/server.py
@@ -5,15 +5,17 @@ serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #bind參數是一個tuple 放主機名，埠號
 serverSocket.bind((socket.gethostname(), 1234, ))
 
+#最多等待5個連線
 serverSocket.listen(5)
+while True:
+    conn, addr = serverSocket.accept()
+    print('connect from', addr)
 
-conn, addr = serverSocket.accept()
-print('connect from', addr)
+    #讀取1024byte
+    data = conn.recv(1024)
 
-data = conn.recv(1024)
+    conn.send(data)
 
-conn.send(data)
-
-conn.close()
+    conn.close()
 
 serverSocket.close()
